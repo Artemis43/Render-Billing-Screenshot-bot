2f6786e55fe16ae198f161da2a6cd789d18fd403
Artemis43 <lohithcodes@gmail.com>
2024-08-01 18:42:11 +0530

    Update install_playwright.py

--- install_playwright.py
+++ install_playwright.py
@@ -1,7 +1,10 @@
 import subprocess
 
-def install_playwright_dependencies():
+def install_playwright_dependencies1():
     subprocess.run(["playwright", "install"], check=True)
 
+def install_playwright_dependencies2():
+    subprocess.run(["playwright", "install-deps"], check=True)
+
 if __name__ == '__main__':
-    install_playwright_dependencies()
+    install_playwright_dependencies1(), install_playwright_dependencies2()
